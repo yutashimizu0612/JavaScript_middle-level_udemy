5bcc8b9a7965d6a36a5a7ee11a64ccac3bf32a6c
清水雄大 <>
2020-02-18 21:50:52 +0900

    コンストラクタを使った場合のthisは、インスタンス化されたオブジェクトを指す（+ 削除した2回分の内容を復活）

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+//============================================
+// thisは、オブジェクトを参照する時に使用される。
+//============================================
+let myObj = {
+  id: 2,
+  printId() {
+    // このthisは、この関数が定義されているオブジェクトを指している。
+    console.log(this.id);
+  }
+}
+myObj.printId(); // 2
+
+//============================================
+// thisは、呼ばれる場所で参照するオブジェクトが変わる。
+//============================================
 const sayFoo = function() {
   console.log(this['foo']);
 }
@@ -11,9 +26,28 @@ const mySecondObj = {
   sayFoo
 }
 
-// globalオブジェクトがthisとなるため、global変数であるfooが呼ばれる。
+// 【1】globalオブジェクトがthisとなるため、global変数であるfooが呼ばれる。
 sayFoo();
-
-// mySecondObjというオブジェクトの中で呼ばれるため、thisが指すのは、mySecondObj。
+// 【2】mySecondObjというオブジェクトの中で呼ばれるため、thisが指すのは、mySecondObj。
 // よって、mySecondObj.fooが呼ばれる。
 mySecondObj.sayFoo();
+
+
+
+//===============================
+// コンストラクタ関数
+//===============================
+function MyObj(id) {
+  // このthisは、のちに生成されるインスタンス化したオブジェクトを指す。
+  this.id = id;
+}
+
+// プロトタイプで関数を定義
+MyObj.prototype.printId = function(id) {
+  console.log(this.id);
+}
+
+// インスタンス化
+// コンストラクタ関数内で定義されているthisは、インスタンスとして生成されたオブジェクトのことを指す。
+const newInstance = new MyObj(22);
+newInstance.printId();

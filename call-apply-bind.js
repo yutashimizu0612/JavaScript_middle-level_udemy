17b615a375481935113954c2ef16412fd3c82f9e
清水雄大 <>
2020-02-20 07:43:57 +0900

    applyメソッド

--- call-apply-bind.js
+++ call-apply-bind.js
@@ -42,3 +42,36 @@
   // let playersArray = [].slice.call(players); // 当然これでもok
   console.log(playersArray);
 }());
+
+
+
+//============================================
+// apply
+//============================================
+// ☆ applyも基本的にcallと同様 ☆
+(function() {
+  function greet() {
+    console.log('apply-arguments', arguments);
+    let hi = `Hi, ${this.name}`
+    console.log(hi);
+  }
+
+  let obj = {
+    name: "ERIKO"
+  };
+
+  // applyの場合は、配列で第2引数に渡してあげる。
+  // 使い分けとしては、配列で渡す場合はapplyを使い、その以外の場合はcallを使う。
+  greet.apply(obj, [1, 2, 3, 4]);
+
+  // applyの使い所
+  const array = [1, 2 ,3 ,4, 5];
+  console.log(Math.min(array));// 配列を展開しないとNaNになってしまう
+  console.log(Math.min(1, 2, 3));// つまりこうする必要がある。
+  // そこでapplyを使って関数を実行する。
+  // applyの第1引数はthisを決めるものなのでnull、第2引数以降に対象の配列を渡す
+  console.log(Math.min.apply(null, array));
+  // ES6からはSpread Operatorを使う
+  console.log(Math.min(...array));
+
+}());

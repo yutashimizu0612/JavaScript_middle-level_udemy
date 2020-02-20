4c3aaf8ddc751b2dafbef9328095275d6bcd3f47
清水雄大 <>
2020-02-21 08:43:16 +0900

    アロー関数によるthisのbind

--- arrow-function.js
+++ arrow-function.js
@@ -9,8 +9,7 @@
 // ☆ 1行しかない場合は、{}とreturnは削除できる ☆
 const timesTwo = i => i * 2;
 
-const res = timesTwo(2);
-console.log(res);
+const res = timesTwo(2); // 4
 
 
 // ☆ 書き方まとめ ☆
@@ -24,3 +23,32 @@ arrowFunc = (x, y) => {
   console.log(z);
   return z;
 }
+
+
+//============================================
+// アロー関数によるthisのbind
+//============================================
+const normalFunc = {
+  id: 5,
+  counter: function() {
+    // ここでのthisは、sampleFuncになる
+    window.setTimeout(function() {
+      console.log(this.id); // undefined（thisはwindowオブジェクトだから）
+    }, 1000);
+  }
+}
+
+// setTimeout内でも、同じthisを呼びたい場合
+const arrowFunc2 = {
+  id: 5,
+  counter: function() {
+    // setTimeoutのcallback関数内のthisは、arrowFunc2オブジェクトを参照する。
+    // アロー関数の場合、thisはアロー関数内で定義されない。すると、スコーンチェーンを辿って、thisを探しに行く。
+    // 最終的にはglobalスコープを探して、その変数（ここではthis）が存在しなければ、undefinedを返すという仕組み。
+    window.setTimeout(() => {
+      console.log(this.id);
+    }, 1000);
+  }
+}
+
+arrowFunc2.counter();

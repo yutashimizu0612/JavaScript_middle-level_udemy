bb09183741cf8a10a087b457f019b54a0b198dce
清水雄大 <>
2020-02-27 22:16:27 +0900

    Promiseを用いた処理

--- asynchronous.js
+++ asynchronous.js
@@ -35,7 +35,59 @@ function wait(num) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log(num);
-      resolve(num);
+      if (num === 4) {
+        reject();
+      } else {
+        resolve(num);
+      }
     }, num);
   });
 };
+
+wait(0).then(num => {
+  num++;
+  // Promiseのthenを使用して、処理を繋げたい場合は、returnでPromiseを返す必要がある
+  return wait(num);
+}).then(num => {
+  num++;
+  return wait(num);
+}).then(num => {
+  num++;
+  return wait(num);
+}).then(num => {
+  num++;
+  return wait(num);
+}).then(num => {
+  num++;
+  return wait(num);
+}).catch(() => {
+  console.error('error');
+})
+
+//============================================
+// Promise.all
+//============================================
+Promise.all(
+  // Promiseを配列で渡してあげると、この関数が全て実行完了した時に、thenメソッドが実行される
+  // この場合は、wait(3000)がもっとも処理が掛かるので、この処理が終わった段階で、thenメソッドが実行される
+  [
+    wait(1000),
+    wait(2000),
+    wait(3000)
+  // numsは、それぞれのPromiseが返すresolveの配列
+  ]).then(nums => {
+    console.log(nums); // [1000, 2000, 3000]
+});
+
+//============================================
+// Promise.race
+//============================================
+Promise.race(
+  // Promiseを配列で渡すと、いずれか1つの関数が実行完了した時に、thenメソッドが実行される
+  [
+    wait(1500),
+    wait(2500),
+    wait(3500)
+  ]).then(nums => {
+    console.log(nums + 1);
+  });
